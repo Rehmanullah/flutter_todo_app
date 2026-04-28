4a48414ca4cc84eedb5bb534f0a5783ac9251281
Rehman <rehmanphdar@gmail.com>
2026-04-28 09:36:27 +0800

    Add new task page

--- lib/new_task_page.dart
+++ lib/new_task_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+
 class NewTaskPage extends StatelessWidget {
   const NewTaskPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton.filled(onPressed: (){
+          Navigator.pop(context);
+        }, icon: Icon(Icons.arrow_back_ios_new_rounded)),
+        title: Text(
+          "New Task",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
       body: Center(child: Text("New Page")),
     );
   }
